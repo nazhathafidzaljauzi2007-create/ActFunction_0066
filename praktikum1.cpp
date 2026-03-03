9c6bd185689aab07780bac8805dba4ce816a0bdb
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-03-03 15:46:42 +0700

    Add files via upload

---
+++ praktikum1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+      float p,l;
+
+void Input (){
+    cout << "Masukkan Panjang : " ;
+    cin >> p;
+    cout << "Masukkan Lebar : " ;
+    cin >> l;
+}
+
+float LuasPersegi(){
+    return p*l;
+}
+
+void Output (){
+    cout << "Hasilnya = " << LuasPersegi();
+}
+
+int main()
+{
+    Input();
+    Output();
+}
---
+++ praktikum2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+      float p,l;
+
+void Input (){
+    cout << "Masukkan Panjang : " ;
+    cin >> p;
+    cout << "Masukkan Lebar : " ;
+    cin >> l;
+}
+
+float LuasPersegi(float x, float y){
+    return x*y;
+}
+
+int Jumlah(int a, int b, int c){
+    return a*b*c;
+}
+
+void Output (){
+    cout << "Hasilnya = " << LuasPersegi(p,l) <<endl;
+}
+
+int main()
+{
+    Input();
+    Output();
+    cout << "Hasil Penjumlahan = " << Jumlah(5,6,9);
+}
